3baed60d0eca6dc64642130a62479d3fe62025d1
Shwabade Xaba <ShwabadeX@hollywoodbets.net>
2025-04-30 15:10:06 +0200

    Fixed formatting for question 10-13

--- sql/QueryScripts/Question12SelectLinkedData.sql
+++ sql/QueryScripts/Question12SelectLinkedData.sql
@@ -1,9 +1,14 @@
-SELECT Title,Genre,Name AS Director_Name
+SELECT 
+  Title,
+  Genre,
+  Name 
 FROM Movie
 JOIN Director
 ON Movie.DirectorID = Director.DirectorID;
 
-SELECT Title,Name
+SELECT 
+  Title,
+  Name
 FROM Movie
 JOIN Director
 ON Movie.DirectorID = Director.DirectorID
--- sql/QueryScripts/Question13SelectAndGroupData.sql
+++ sql/QueryScripts/Question13SelectAndGroupData.sql
@@ -1,12 +1,15 @@
-SELECT SUM(TotalWatchTime) AS Total_Watch_Time_Of_All_Movies
+SELECT 
+  SUM(TotalWatchTime) AS Total_Watch_Time_Of_All_Movies
 FROM Movie;
 
-SELECT	Genre,
-		SUM(TotalWatchTime) AS Total_Watch_Time
+SELECT	
+  Genre,
+  SUM(TotalWatchTime) AS Total_Watch_Time
 FROM Movie
 GROUP BY Genre;
 
-SELECT Genre
+SELECT 
+  Genre
 FROM Movie
 GROUP BY Genre
 HAVING COUNT(MovieID) > 1;
